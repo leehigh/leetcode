c7981f5e2ed1488b3d260af590c998281761e6c1
leehigh <ljchnsam@gmail.com>
2018-05-17 15:05:40 +0800

    two sum

---
+++ twoSum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        need = {}
+        for i in range( len(nums)):
+            if nums[i] in need:
+                return [need[nums[i]], i]
+            else:
+                need[target - nums[i]] = i
